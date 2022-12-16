533f418721f9e30ce511a894e2ac28d7e8b40f88
Jonathan Teoh Kai Xiang <jonathan.teohkx@gmail.com>
2022-12-16 20:06:32 +0800

    change function type in CNN from <int> to <float>

--- CNN/CNN/CNN.cpp
+++ CNN/CNN/CNN.cpp
@@ -3,35 +3,48 @@
 #include <stdio.h>
 #include <omp.h>
 #include <vector>
+#include "Numpy.h"
+#include <cmath>
 
 using namespace std;
 
-vector<vector<int>> conv_(vector<vector<float>> img, vector<vector<float>> conv_filter)
+using namespace MathLibrary;
+
+vector<vector<float>> conv_(vector<vector<float>> img, vector<vector<float>> conv_filter)
 {
+	vector<vector<float>> conv_result;
+
 	vector<vector<int>> b;
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
 			b[i].push_back(0);
 		}
 	}
+	return conv_result;
 }
 
-vector<vector<int>> conv(vector<vector<float>> img, vector<vector<float>> conv_filter)
+vector<vector<float>> conv(vector<vector<float>> img, vector<vector<float>> conv_filter)
 {
+	vector<vector<float>> convresult;
 
+	return convresult;
 }
 
-vector<vector<vector<int>>> pooling(vector<vector<vector<float>>> feature_map, int size = 2, int stride = 2)
+vector<vector<vector<float>>> pooling(vector<vector<vector<float>>> feature_map, int size = 2, int stride = 2)
 {
+	vector<vector<vector<float>>> poolingresult;
 
+	return poolingresult;
 }
 
-vector<vector<vector<int>>> relu(vector<vector<vector<float>>> feature_map)
+vector<vector<vector<float>>> relu(vector<vector<vector<float>>> feature_map)
 {
+	vector<vector<vector<float>>> reluresult;
 
+	return reluresult;
 }
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	
 }
--- CNN/CNN/Numpy.cpp
+++ CNN/CNN/Numpy.cpp
@@ -1,6 +1,7 @@
 #include "Numpy.h"
 #include <iostream>
 #include <cmath>
+#include <omp.h>
 // MathLibrary.cpp
 // compile with: cl /c /EHsc MathLibrary.cpp
 // post-build command: lib MathLibrary.obj
@@ -17,46 +18,66 @@ namespace MathLibrary
         vector<vector<float>> result(ROW_COUNT,vector<float>(COLUMN_COUNT));
         return result;
     }
+
     vector<vector<float>>  Numpy:: arange(vector<vector<float>> input) {
-    
+        vector <vector<float>> result(input.size());
+
+        return result;
     }
+
     vector<vector<float>>  Numpy:: Ceil(vector<vector<float>> input) {
         vector <vector<float>> result(input.size());
+#pragma omp parallel for
         for (int i = 0; i < input.size(); i++) {
             result[i] = Ceil(input[i]);
         }
+#pragma omp barrier
         return result;
     }
+
     vector<float> Numpy::Ceil(vector<float> input) {
         vector <float> result(input.size());
+#pragma omp parallel for
         for (int i = 0; i < input.size(); i++) {
             result[i] = ceil(input[i]);
         }
+#pragma omp barrier
         return result;
     }
+
     vector<vector<float>>  Numpy::Floor(vector<vector<float>> input) {
         vector <vector<float>> result(input.size());
+#pragma omp parallel for
         for (int i = 0; i < input.size(); i++) {
             result[i] = Floor(input[i]);
         }
+#pragma omp barrier
         return result;
     }
+
     vector<float>  Numpy::Floor(vector<float> input) {
         vector<float>result(input.size());
+#pragma omp parallel for
         for (int i = 0; i < input.size();i++) {
             result[i] = floor(input[i]);
         }
+#pragma omp barrier
         return result;
     }
+
     vector<vector<float>> Numpy::Max(vector<vector<float>> input, int value) {
         vector<vector<float>>result(input.size());
+#pragma omp parallel for
         for (int i = 0; i < input.size(); i++) {
                 result[i]  = Max(input[i], value);
         }
+#pragma omp barrier
         return result;
     }
+
     vector<float>  Numpy::Max(vector<float> input, int value) {
         vector<float>result(input.size());
+#pragma omp parallel for
         for (int i = 0; i < input.size(); i++) {
             if (input[i] >= value) {
                 result[i] = input[i];
@@ -65,5 +86,7 @@ namespace MathLibrary
                 result[i] = value;
             }
         }
+#pragma omp barrier
+        return result;
     }
 }
