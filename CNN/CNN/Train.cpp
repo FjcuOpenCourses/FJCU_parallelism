93e56c8a4021bc075ab152d401742afb2f30f873
DanielDu <daniel@jasslin.com>
2022-12-18 05:02:07 +0800

    add train

--- CNN/CNN/Train.cpp
+++ CNN/CNN/Train.cpp
@@ -6,7 +6,7 @@
 #include    "Flatten.h"
 #include    "Dense.h"
 #include    "Model.h"
-
+#include <iostream>
 
 using namespace std;
 
@@ -22,32 +22,19 @@ int main() {
     Conv2D              conv_layer3(1, 3, max_pooling_layer, NULL);
     ReLU                relu_layer3(conv_layer3);
     AveragePooling2D    pooling_layer(2, relu_layer3, 2);
-
-    Flatten flatten_layer(previous_layer = pooling_layer)
-    Dense dense_layer1(num_neurons = 100,
-            previous_layer = flatten_layer,
-            activation_function = "relu")
-        dense_layer2 = pygad.cnn.Dense(num_neurons = num_classes,
-            previous_layer = dense_layer1,
-            activation_function = "softmax")
-
-        model = pygad.cnn.Model(last_layer = dense_layer2,
-            epochs = 1,
-            learning_rate = 0.01)
-
-        model.summary()
-
-        model.train(train_inputs = train_inputs,
-            train_outputs = train_outputs)
-
-        predictions = model.predict(data_inputs = train_inputs)
-        print(predictions)
-
-        num_wrong = numpy.where(predictions != train_outputs)[0]
-        num_correct = train_outputs.size - num_wrong.size
-        accuracy = 100 * (num_correct / train_outputs.size)
-        print("Number of correct classifications : {num_correct}.".format(num_correct = num_correct))
-        print("Number of wrong classifications : {num_wrong}.".format(num_wrong = num_wrong.size))
-        print("Classification accuracy : {accuracy}.".format(accuracy = accuracy))
+    Flatten             flatten_layer(pooling_layer);
+    Dense               dense_layer1(100,flatten_layer,"relu");
+    Dense               dense_layer2(num_classes, dense_layer1,"softmax");
+    Model               model(dense_layer2, 1, 0.01);
+    model.summary();
+    model.train(train_inputs = train_inputs, train_outputs = train_outputs);
+    predictions = model.predict(data_inputs = train_inputs);
+    print(predictions);
+    int num_wrong = numpy.where(predictions != train_outputs)[0];
+        int num_correct = train_outputs.size - num_wrong.size;
+    int accuracy = 100 * (num_correct / train_outputs.size);
+    printf("Number of correct classifications : {%d}.", num_correct);
+    printf("Number of wrong classifications : {%d}.",num_wrong.size);
+    printf("Classification accuracy : {%.l2f}.",accuracy);
 	return 0;
 }
