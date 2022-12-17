f9752c5584ff3f945ed46e05036ce745fbb50aad
juchien-admin <a1198878821@gmail.com>
2022-12-17 18:08:14 +0800

    delte eror

--- CNN/CNN/CNN.cpp
+++ CNN/CNN/CNN.cpp
@@ -11,64 +11,8 @@ using namespace std;
 
 CNN::CNN() {
 
-    <<<<<< < HEAD
-        input2D : The input to be convolved by the filter bank.
-        The conv() method saves the result of convolving the input by the filter bank in the layer_output attribute.
-        * /
-
-        /*
-            int rows = 3;
-            int columns = 4;
-            vector<vector<int>> vec1(rows , vector<int> (columns));
-
-            int rows = ans[0].size();  //rows
-            int column = ans.size();   //columns
-        */
-
-        // conv first if
-        int input_dimension, initial_weights_shapeHandle;
-    for (int i = 0; i < 10; i++)
-    {
-        if (input2D.size[]() > 1)
-        {
-
-        }
-    }
-    if (input2D.size() > 1 && input2D[0].size() > 1)
-        input_dimension = 2;
-    else
-        input_dimension = 1;
-    /*
-    # Initializing the filters of the conv layer.
-        self.initial_weights = numpy.random.uniform(low=-0.1,
-                                                    high=0.1,
-                                                    size=self.filter_bank_size)
-    */
-    if (this->initial_weights.size() > 1 && this->initial_weights[0].size() > 1)
-        initial_weights_shapeHandle = 1;
-    else if (this->initial_weights.size() == 1 && this->initial_weights[0].size() > 1)
-        initial_weights_shapeHandle = 0;
-    if (input_dimension != initial_weights_shapeHandle) // # Check if there is a match in the number of dimensions between the imageand the filters.
-        cout << "Number of dimensions in the conv filter and the input do not match." << endl;
 
 
-    // conv second if
-
-
-
-        // if len(input2D.shape) != len(self.initial_weights.shape) - 1 : # Check if there is a match in the number of dimensions between the imageand the filters.
-            // raise ValueError("Number of dimensions in the conv filter and the input do not match.")
-    if len(input2D.shape) > 2 or len(self.initial_weights.shape) > 3: # Check if number of image channels matches the filter depth.
-        if input2D.shape[-1] != self.initial_weights.shape[-1]:
-    raise ValueError("Number of channels in both the input and the filter must match.")
-        if self.initial_weights.shape[1] != self.initial_weights.shape[2] : # Check if filter dimensions are equal.
-            raise ValueError('A filter must be a square matrix. I.e. number of rows and columns must match.')
-            if self.initial_weights.shape[1] % 2 == 0: # Check if filter diemnsions are odd.
-                raise ValueError('A filter must have an odd size. I.e. number of rows and columns must be odd.')
-
-                self.layer_output = self.conv_(input2D, self.trained_weights)
-                ====== =
-                >>>>>> > 0ea7ccf925e536087a4ec1c8e1ced99d13528f72
 }
 vector<vector<float>> CNN::conv_(vector<vector<float>> img, vector<vector<float>> conv_filter) {
     vector<vector<float>> conv_result;
